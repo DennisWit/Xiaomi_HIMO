9d8f77752592f8bea1d77c706741b80d903a45d8
DennisWit <117594384+DennisWit@users.noreply.github.com>
2023-11-18 23:11:40 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    $('.bike__slider, .slider__items').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplayspeed: 2000,
+    });
+
+});
+
+
+
